5e5a5c2aa77fc7c18aa6d695b1d6faa117d4d106
James Johnes <jamesdabone@gmail.com>
2022-09-03 04:09:09 -0400

    cleanup

--- src/containers/Updater/index.js
+++ src/containers/Updater/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
