75b9eb0af4b036bc4069dfd7d773ddfe7b78e28b
James Johnes <jamesdabone@gmail.com>
2022-09-03 13:02:35 -0400

    cleanup

--- src/containers/SideDrawer/SideDrawer.js
+++ src/containers/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { styled, useTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
