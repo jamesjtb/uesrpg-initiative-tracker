3090fc388075f5d393e3b21e2e80fbf3c3667745
James Johnes <jamesdabone@gmail.com>
2023-12-15 02:49:58 -0500

    remove unused useEffect

--- src/containers/Encounters/EncounterList/EncounterList.js
+++ src/containers/Encounters/EncounterList/EncounterList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
