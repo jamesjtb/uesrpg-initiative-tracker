0bda7fc6687d93d6f1ac21c58a69fb61b9685f43
James Johnes <jamesdabone@gmail.com>
2024-02-17 01:24:27 -0500

    some minor progress

--- src/containers/Encounters/EncounterBuilder/EncounterBuilder.js
+++ src/containers/Encounters/EncounterBuilder/EncounterBuilder.js
@@ -8,43 +8,75 @@ import Typography from '@mui/material/Typography';
 import { StyledTableCell } from '../../../components/StyledComponents/TableCell';
 import { EncounterContext } from '../../../contextProviders/activeEncounter';
 import CombatantActions from './CombatantActions';
+import { Box, Divider, Grid, IconButton, Tooltip } from '@mui/material';
+import { Save } from '../../../components/rpg-awesome/electronics';
 
 const EncounterBuilder = () => {
     const { encounterState } = useContext(EncounterContext);
     const [npcStatblocks, setNpcStatblocks] = useState({});
-    
+
     useEffect(() => {
         (async () => {
             for (const combatant of encounterState.combatants) {
                 const statblock = await window.bestiary.getOne(combatant.npcId);
-                if (encounterState?.combatants) setNpcStatblocks(n => ({...n, [combatant.npcId]: statblock}));
+                if (encounterState?.combatants)
+                    setNpcStatblocks(n => ({ ...n, [combatant.npcId]: statblock }));
             }
         })();
     }, [encounterState.combatants]);
 
     return (
-        <Table size="small">
-            <TableHead>
-                <TableRow>
-                    <StyledTableCell align="left">Name</StyledTableCell>
-                    <StyledTableCell align="right">Label Color</StyledTableCell>
-                    <StyledTableCell align="right" />
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {encounterState?.combatants?.map(combatant => (
-                    <TableRow key={combatant.id}>
-                        <StyledTableCell align="left">
-                            <Typography component="span">{combatant.name}</Typography>
-                        </StyledTableCell>
-                        <StyledTableCell align="right">
-                            <Typography component="span">something</Typography>
-                        </StyledTableCell>
-                        <StyledTableCell align="right"><CombatantActions combatant={combatant} statblock={npcStatblocks[combatant.npcId]} /></StyledTableCell>
+        <Box sx={{ height: '30vh', overflow: 'auto', width: '100%' }}>
+            <Box sx={{ width: '100%' }}>
+                <Divider>
+                    <Typography variant="h5" color="primary">
+                        Encounter Builder
+                    </Typography>
+                </Divider>
+                <Grid container>
+                    <Grid
+                        item
+                        xs={10}
+                        textAlign="left"
+                        sx={{ '& > :not(style)': { mr: 1, ml: 1 } }}
+                    ></Grid>
+                    <Grid item xs={2} textAlign="right">
+                        <Tooltip title="Add New NPC">
+                            <IconButton color="primary" onClick={() => []}>
+                                <Save fontSize="inherit" />
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell align="left">Name</StyledTableCell>
+                        <StyledTableCell align="right">Label Color</StyledTableCell>
+                        <StyledTableCell align="right" />
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHead>
+                <TableBody>
+                    {encounterState?.combatants?.map(combatant => (
+                        <TableRow key={combatant.id}>
+                            <StyledTableCell align="left">
+                                <Typography component="span">{combatant.name}</Typography>
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                                <Typography component="span">something</Typography>
+                            </StyledTableCell>
+                            <StyledTableCell align="right">
+                                <CombatantActions
+                                    combatant={combatant}
+                                    statblock={npcStatblocks[combatant.npcId]}
+                                />
+                            </StyledTableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Box>
     );
 };
 
--- src/containers/Encounters/Encounters.js
+++ src/containers/Encounters/Encounters.js
@@ -1,51 +1,19 @@
 import React, { useState } from 'react';
 
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
-
-import Add from '@mui/icons-material/Add';
-
-import { openChildWindow } from '../../util/utils';
 import EncounterBuilder from './EncounterBuilder/EncounterBuilder';
+import { Typography } from '@mui/material';
 
 const Encounters = () => {
     const [encounters, setEncounters] = useState([]);
 
     return (
         <Container sx={{ flexGrow: 1 }}>
+            <Typography variant="h4" color="primary">
+                Encounters
+            </Typography>
             <Stack alignItems="center" justifyContent="center">
-                <Box sx={{ mb: 2, width: '100%' }}>
-                    <Typography variant="h4" color="primary">
-                        Encounter Builder
-                    </Typography>
-                    <Grid container>
-                        <Grid
-                            item
-                            xs={10}
-                            textAlign="left"
-                            sx={{ '& > :not(style)': { mr: 1, ml: 1 } }}
-                        ></Grid>
-                        <Grid item xs={2} textAlign="right">
-                            <Tooltip title="Add New NPC">
-                                <IconButton
-                                    color="primary"
-                                    onClick={() =>
-                                        openChildWindow('views/encounterbuilder', {
-                                            modal: true,
-                                        })
-                                    }
-                                >
-                                    <Add fontSize="inherit" />
-                                </IconButton>
-                            </Tooltip>
-                        </Grid>
-                    </Grid>
-                </Box>
                 <EncounterBuilder encounters={encounters} />
             </Stack>
         </Container>
